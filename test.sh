c0e614eb84b1f0308fc52ff91af81b81a15f6ddb
Jayanth Akash <jayanthakash.ve@gmail.com>
2023-10-13 14:54:33 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,12 @@
-rosdep install -y --from-paths src --ignore-src --rosdistro noetic -r --os=debian:buster
-sudo dphys-swapfile setup
-sudo dphys-swapfile swapon
-sudo src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release --install-space /opt/ros/noetic -j1 -DPYTHON_EXECUTABLE=/usr/bin/python
+sudo apt-get install python3-rosdep python3-rosinstall-generator python3-vcstools python3-vcstool build-essential
+sudo python -m pip install --upgrade setuptools
+sudo python -m pip install -U rosdep rosinstall_generator vcstool
+sudo rosdep init
+rosdep update
+mkdir ~/ros_catkin_ws
+cd ~/ros_catkin_ws
+rosinstall_generator ros_base --rosdistro noetic --deps --tar > noetic-ros_base.rosinstall
+mkdir ./src
+vcs import --input noetic-ros_base.rosinstall ./src
+rosdep install --from-paths ./src --ignore-packages-from-source --rosdistro noetic -y
+./src/catkin/bin/catkin_make_isolated --install -DCMAKE_BUILD_TYPE=Release
