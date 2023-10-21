4dfc403d995c58fbaea4b7e0fd07c87258921cb5
Jayanth Akash <jayanthakash.ve@gmail.com>
2023-10-21 13:24:57 +0530

    Update get_grayscale_values.py

--- get_grayscale_values.py
+++ get_grayscale_values.py
@@ -6,8 +6,11 @@ import os
 
 px = Picarx()
 #print( px.line_reference,"reference")
-while True:
-  gm_val_list = px.get_grayscale_data()
-  print("gm_val_list:",gm_val_list)
-  gm_status = px.get_line_status(gm_val_list)
-  print("gm_status:",gm_status)
+try:
+  while True:
+    gm_val_list = px.get_grayscale_data()
+    print("gm_val_list:",gm_val_list)
+    gm_status = px.get_line_status(gm_val_list)
+    print("gm_status:",gm_status)
+except:
+  px.stop()
